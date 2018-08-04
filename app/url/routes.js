89a80ea3454c06d7fdf06af78ba3ccc59cff4bf7
Mark Hkr <markhker@gmail.com>
2018-08-03 22:59:47 -0500

    Added index, 404 error on hash not exists, register visit & error messag

--- app/url/routes.js
+++ app/url/routes.js
@@ -1,17 +1,25 @@
 const router = require('express').Router();
 const url = require('./url');
+var path = require('path');
 
+router.get('/', async (req, res) => {
+  // route to serve up the homepage (index.html)
+  res.sendFile(path.join(__dirname, 'views/index.html'));
+});
 
-router.get('/api/item/:hash', async (req, res) => {
+router.get('/:hash', async (req, res, next) => {
 
   const source = await url.getUrl(req.params.hash);
 
   // TODO: Respond accordingly when the hash wasn't found (404 maybe?)
+  if(!source){
+		let err = new Error('That hash does not exist');
+		err.status = 404;
+		next(err);
+		return;
+	}
 
   // TODO: Hide fields that shouldn't be public
-
-  // TODO: Register visit
-
 
   // Behave based on the requested format using the 'Accept' header.
   // If header is not provided or is */* redirect instead.
@@ -25,16 +33,17 @@ router.get('/api/item/:hash', async (req, res) => {
       res.json(source);
       break;
     default:
+      // TODO: Register visit
+      url.sumVisit(req.params.hash);
       res.redirect(source.url);
       break;
   }
 });
 
 
-router.post('/api/item', async (req, res, next) => {
+router.post('/api/shorten', async (req, res, next) => {
 
   // TODO: Validate 'req.body.url' presence
-
   if(typeof req.body.url != "string"){
 		let err = new Error('URL is a required parameter');
 		err.status = 400;
@@ -45,9 +54,11 @@ router.post('/api/item', async (req, res, next) => {
   try {
     let shortUrl = await url.shorten(req.body.url);
     res.json(shortUrl);
-  } catch (e) {
+  } catch (err) {
     // TODO: Personalized Error Messages
-    next(e);
+    if (!err.statusCode) err.statusCode = 500;
+    res.status(err.statusCode).send(err.message);
+    next(err);
   }
 });
 
